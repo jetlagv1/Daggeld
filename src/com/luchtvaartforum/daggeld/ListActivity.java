2b9571f84b332e7d07fec8de98bd8ef66d9ee2b7
jetlagv1 <marc.damen+github@gmail.com>
2014-09-22 14:53:58 +0200

    1

--- src/com/luchtvaartforum/daggeld/ListActivity.java
+++ src/com/luchtvaartforum/daggeld/ListActivity.java
@@ -1,23 +1,19 @@
 package com.luchtvaartforum.daggeld;
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.app.*;
-import android.text.*;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.DialogFragment;
+import android.app.Activity;
 import android.os.Bundle;
-import android.app.*;
-import android.text.*;
-import android.widget.ListView;
-import android.widget.BaseAdapter;
-import android.widget.TextView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
+public class ListActivity extends Activity {
 
-
-public class ListActivity extends Activity
-{
-	
-	static String[] words = {"one", "two", "three"};
+static String[] words = {"one", "two", "three"};
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,4 +41,29 @@ public class ListActivity extends Activity
 					return view;
 			}});
 		}
+//	@Override
+//	protected void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_list);
+//	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.list, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
 }
